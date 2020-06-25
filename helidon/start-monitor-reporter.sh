9b9a886d895461de4a96403767da342aad9747e0
tkote <7146253+tkote@users.noreply.github.com>
2020-06-25 16:18:02 +0900

    adopted Coherence CE

--- helidon/start-monitor-reporter.sh
+++ helidon/start-monitor-reporter.sh
@@ -2,9 +2,8 @@
 
 # change the path for the nosql & coherence driver
 NOSQL_DRIVER="/home/opc/work/nosql/sdk/lib/nosqldriver.jar"
-COHERENCE_JAR="/home/opc/opt/wls1411/coherence/lib/coherence.jar"
 
-java -cp target/tempmon.jar:$NOSQL_DRIVER:$COHERENCE_JAR \
+java -cp target/tempmon.jar:$NOSQL_DRIVER \
     -Dslack-alerter.enabled=false -Dmonitor-store=$1 \
     oracle.demo.tempmon.Main
 
--- helidon/start-reporter.sh
+++ helidon/start-reporter.sh
@@ -2,8 +2,7 @@
 
 # change the path for the nosql & coherence driver
 NOSQL_DRIVER="/home/opc/work/nosql/sdk/lib/nosqldriver.jar"
-COHERENCE_JAR="/home/opc/opt/wls1411/coherence/lib/coherence.jar"
 
-java -cp target/tempmon.jar:$NOSQL_DRIVER:$COHERENCE_JAR \
+java -cp target/tempmon.jar:$NOSQL_DRIVER \
     -Dsimulator.enabled=false -Dslack-alerter.enabled=false -Dmonitor-store=$1 \
     oracle.demo.tempmon.Main
